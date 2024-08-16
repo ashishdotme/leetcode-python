787df979a4f8f947e8df6ce50f8110a9e5b54ef4
Ashish Patel <ashishsushilpatel@gmail.com>
2024-08-16 23:14:59 +0100

    feat(array): add

--- Array/Easy/0027-remove-element/solution.py
+++ Array/Easy/0027-remove-element/solution.py
@@ -81,7 +81,13 @@ import string
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
+
 
 # @lc code=end
 
